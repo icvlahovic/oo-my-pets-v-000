0222004cfefff25da66d80142a5e91eb87ce2ce4
Ian Vlahovic <banddorkfht7@gmail.com>
2018-03-19 01:10:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,7 +56,8 @@ class Owner
 
   def sell_pets
     @pets.each do |species|
-      species.each { |pet| pet.mood=("nervous") }
+      puts species
+      # species.each { |pet| pet.mood=("nervous") }
     end
   end
 
